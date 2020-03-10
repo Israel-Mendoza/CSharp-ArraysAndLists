46cb39c69dbc22ea8c09405d485b468b08f91727
Israel Mendoza <xander_irsl@hotmail.com>
2020-03-09 22:14:13 -0600

    Added project with different Array metyhods (sorting, reversing, comparing)

---
+++ Arrays2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFriends = { "Victor", "Fernando", "Miguel", "Sergio", "Claudio" };
+            string[] myFriends2 = { "Victor", "Fernando", "Miguel", "Sergio", "Claudio" };
+            int[] myGrades = { 1, 5, 95, 2, 125 };
+
+            Console.WriteLine("These are the contents of the \"myFriends\" array: ");
+            ArrayPrinter(myFriends);
+
+            Console.WriteLine("These are the contents of the \"myGrades\" array: ");
+            ArrayPrinter(myGrades);
+
+
+            Array.Sort(myFriends);
+
+            Array.Sort(myGrades);
+
+            Console.WriteLine("These are the contents of the \"myFriends\" array after sorting: ");
+            ArrayPrinter(myFriends);
+
+            Console.WriteLine("These are the contents of the \"myGrades\" array after sorting: ");
+            ArrayPrinter(myGrades);
+
+            Array.Reverse(myFriends);
+
+            Array.Reverse(myGrades);
+
+            Console.WriteLine("These are the contents of the \"myFriends\" array after reversing: ");
+            ArrayPrinter(myFriends);
+
+            Console.WriteLine("These are the contents of the \"myGrades\" array after reversing: ");
+            ArrayPrinter(myGrades);
+
+            Console.WriteLine($"Where is Sergio located in myFriends array? {Array.IndexOf(myFriends, "Sergio")}");
+            Console.WriteLine($"Is Omar in myFriends array? {(Array.IndexOf(myFriends, "Omar") != -1 ? "Yes!" : "Nope!")}");
+            Console.WriteLine($"Is Sergio in myFriends array? {(Array.IndexOf(myFriends, "Sergio") != -1 ? "Yes!" : "Nope!")}\n");
+
+
+            Console.WriteLine($"Is myFriends1 equal to myFriends2? {Enumerable.SequenceEqual(myFriends, myFriends2)}");
+            Array.Sort(myFriends2);
+            Array.Reverse(myFriends2);
+            Console.WriteLine($"Is myFriends1 equal to myFriends2? {Enumerable.SequenceEqual(myFriends, myFriends2)}");
+
+        }
+
+        static void ArrayPrinter(string[] anArray)
+        {
+            foreach (string item in anArray)
+            {
+                Console.WriteLine($"\t{item}");
+            }
+            Console.WriteLine();
+        }
+
+        static void ArrayPrinter(int[] anArray)
+        {
+            foreach (int number in anArray)
+            {
+                Console.WriteLine($"\t{number}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ JaggedArrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring a jagged array
+            int[][] jaggedArray = new int[3][];
+
+            // Declaring each array within the jagged array
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+            // Declaring and initializing each array within the jagged array
+            jaggedArray[0] = new int[] { 1, 2, 3, 4, 5, 6 };
+            jaggedArray[1] = new int[] { 1, 2, 3, 4, 5, 6, 7, };
+            jaggedArray[2] = new int[] { 1, 2, 3 };
+
+            // Declaring and initializing a jagged array
+
+            int[][] jaggedArray2 = new int[][]
+            {
+                new int[]{1, 3, 5},
+                new int[]{2, 4, 6},
+                new int[]{1, 5, 9},
+            };
+
+            foreach (int[] intArray in jaggedArray)
+            {
+                foreach (int num in intArray)
+                {
+                    Console.WriteLine($"Current value in jagged array 1: {num}");
+                }
+            }
+            Console.WriteLine();
+
+            foreach (int[] intArray in jaggedArray2)
+            {
+                foreach (int num in intArray)
+                {
+                    Console.WriteLine($"Current value in jagged array 2: {num}");
+                }
+            }
+        }
+    }
+}
